c26494b8955ccf1998f7f4203dc3cdfd85b50390
Dmanjav <A01753486@tec.mx>
2021-11-25 10:03:19 -0600

    ya

--- Listasobj/triangle.h
+++ Listasobj/triangle.h
@@ -6,9 +6,9 @@ using namespace std;
 
 class Triangle{
     private:
-        Point vertex1{};
-        Point vertex2{};
-        Point vertex3{};
+        Point vertex1{0,0};
+        Point vertex2{50,30};
+        Point vertex3{25,10};
     public:
         Triangle();
         Triangle(Point, Point, Point);
