0146af6735c37d598a1c30cdaebda0d9dd5f6be4
Diego Manja <88682622+A01753486@users.noreply.github.com>
2021-11-29 23:43:50 -0600

    alabestia

--- proyecto/perfil.cpp
+++ proyecto/perfil.cpp
@@ -4,25 +4,31 @@
 
 using namespace std;
 
-Perfil::Perfil(): nombre{"Desconocido"},telefono{"Desconocido"},ciudad{"Desconocida"}{}
-Perfil::Perfil(string nomb,string tel, string city):nombre{nomb},telefono{tel},ciudad{city}{}
+Perfil::Perfil(): nombre{"Anonimo"},telefono{"Desconocido"},ciudad{"Desconocida"}{}
+Perfil::Perfil(string nombre,string telefono, string citx):nombre{nombre},telefono{telefono},ciudad{citx}{}
 
 string Perfil::getNombre(){return nombre;}
 string Perfil::getTelefono(){return telefono;}
 string Perfil::getCiudad(){return ciudad;}
+
 void Perfil::setNombre(string nomb){nombre = nomb;}
-void Perfil::setTelefono(string tel){nombre = tel;}
-void Perfil::setCiudad(string city){nombre = city;}
+void Perfil::setTelefono(string tel){telefono = tel;}
+void Perfil::setCiudad(string city){ciudad = city;}
 
 void Perfil::guardar(){
-    cout << "\n***Tu informacion ha sido guardada.***\n";
+    cout << "\n***La informacion del usuario ha sido guardada.***\n";
 }
 
 void Perfil::agregarAmigos(){
-
+    string amigo;
+    cout << "Por favor, ingrese el nombre de usuario :"; cin >> amigo;
+    cout << "El usuario " + amigo + " no fue encontrado.";
+    cout << "Por favor intente más tarde.";
 }
 
 void Perfil::cum(){
-
+    string cumple;
+    cout << "No se tienen datos sobre el cumpleaños.";
+    cout << "Por favor ingrese su cumpleanios: "; cin >> cumple;
+    cout << "Se han guardado sus datos.";
 }
-
